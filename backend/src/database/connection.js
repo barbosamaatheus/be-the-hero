d83fca1c69b09dd1fea286c30478bfc6b93b8688
barbosamaathues <matheustdd@gmail.com>
2020-03-27 16:08:53 -0300

    ~Adjusts~

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,9 +1,16 @@
 const knex = require("knex");
 const config = require("../../knexfile");
 
-const envConfig =
-  process.env.NODE_ENV === "test" ? config.test : config.development;
+let envConfig = config.production;
 
+if (process.env.NODE_ENV === "test") {
+  envConfig = config.test;
+} else if (process.env.NODE_ENV === "dev") {
+  envConfig = config.development;
+}
+console.log("connection on:", envConfig);
 const connection = knex(envConfig);
+
+connection.migrate.latest();
 
 module.exports = connection;
