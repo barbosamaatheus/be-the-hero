adde455c0db3d30cd2d03b0d173e4b6bd4a13a4b
barbosamaatheus <maatheusb96@gmail.com>
2020-04-29 01:44:29 -0300

    backup backend

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,27 @@
+const connection = require("../database/connection");
+const generateUniqueId = require("../utils/generateUniqueId");
+
+module.exports = {
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
+
+    return res.status(200).json(ongs);
+  },
+
+  async store(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = generateUniqueId();
+
+    await connection("ongs").insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    });
+
+    return res.status(201).json({ id });
+  }
+};
